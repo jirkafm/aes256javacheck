4870717fd22d67298230c6cdf7bc940c2e3af34d
jkaplan <jiri.kaplan@email.cz>
2016-07-28 15:22:27 +0200

    Verbose output (java.home and java.version is printed) if -v or --verbose argument is given.

--- aes256javacheck/src/jirkafm/AES256JavaCheck.java
+++ aes256javacheck/src/jirkafm/AES256JavaCheck.java
@@ -1,6 +1,9 @@
 package jirkafm;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.crypto.Cipher;
 
@@ -12,14 +15,27 @@ import javax.crypto.Cipher;
  */
 public class AES256JavaCheck {
 
+	private static final String JAVA_HOME = "java.home";
+	private static final String JAVA_VERSION = "java.version";
 	private static final String AES_TRANSFORMATION = "AES/CBC/PKCS5Padding";
 	private static final String UNLIMITED_JCE_INSTALLED = "Unlimited JCE policy is active for JVM. JVM can use AES256.";
 	private static final String UNLIMITED_JCE_NOT_INSTALLED = "Unlimited JCE policy has not been applied for JVM. JVM cannot use AES256.";
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
+		Set<String> argsSet = Arrays.stream(args).collect(Collectors.toSet());
+		checkArgs(argsSet);
+
 		int maxAesCipherSize = Cipher.getMaxAllowedKeyLength(AES_TRANSFORMATION);
 		String message = maxAesCipherSize == Integer.MAX_VALUE ? UNLIMITED_JCE_INSTALLED : UNLIMITED_JCE_NOT_INSTALLED;
 		System.out.println(message);
+	}
+
+	private static void checkArgs(Set<String> argsSet) {
+		if (argsSet.contains("-v") || argsSet.contains("--verbose")) {
+			System.out.println(JAVA_HOME + ":\t" + System.getProperty(JAVA_HOME));
+			System.out.println(JAVA_VERSION + ":\t" + System.getProperty(JAVA_VERSION));
+			System.out.println();
+		}
 	}
 
 }
