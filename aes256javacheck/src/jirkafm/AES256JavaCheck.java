631dd8168fa6ae3adc3bafe7d614a18b1e180703
jkaplan <jiri.kaplan@email.cz>
2016-07-28 14:53:38 +0200

    Missing dot.

--- aes256javacheck/src/jirkafm/AES256JavaCheck.java
+++ aes256javacheck/src/jirkafm/AES256JavaCheck.java
@@ -14,7 +14,7 @@ public class AES256JavaCheck {
 
 	private static final String AES_TRANSFORMATION = "AES/CBC/PKCS5Padding";
 	private static final String UNLIMITED_JCE_INSTALLED = "Unlimited JCE policy is active for JVM. JVM can use AES256.";
-	private static final String UNLIMITED_JCE_NOT_INSTALLED = "Unlimited JCE policy has not been applied for JVM. JVM cannot use AES256";
+	private static final String UNLIMITED_JCE_NOT_INSTALLED = "Unlimited JCE policy has not been applied for JVM. JVM cannot use AES256.";
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 		int maxAesCipherSize = Cipher.getMaxAllowedKeyLength(AES_TRANSFORMATION);
